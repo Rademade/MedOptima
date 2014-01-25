a8e328e04270c667fc9ae6e3e0fb2f02d9663e2a
Yaroslav Konoplov <the.yivo@gmail.com>
2014-01-26 01:39:29 +0200

    small fixes

--- public/s/public/js/map.js
+++ public/s/public/js/map.js
@@ -1,34 +1,29 @@
 $(function(){
 
-    var myMap;
+    if ( ymaps !== undefined ) {
+        var myMap;
 
-    ymaps.ready(init);
+        ymaps.ready(init);
 
-    function init () {
-        myMap = new ymaps.Map("map-canvas", {
-            center: [51.609809,46.013142],
-            zoom: 15
-        });
+        function init () {
+            myMap = new ymaps.Map("map-canvas", {
+                center: [51.609809,46.013142],
+                zoom: 15
+            });
 
-        myMap.controls.add('zoomControl').add('typeSelector').add('mapTools');
+            myMap.controls.add('zoomControl').add('typeSelector').add('mapTools');
 
-        var myPlacemark = new ymaps.GeoObject({
-            geometry: {
-                type: "Point",
-                coordinates: [51.609809,46.013142]
-            }});
+            var myPlacemark = new ymaps.GeoObject({
+                geometry: {
+                    type: "Point",
+                    coordinates: [51.609809,46.013142]
+                }});
 
-        var myPlacemark = new ymaps.Placemark([51.609809,46.013142]);
+            var myPlacemark = new ymaps.Placemark([51.609809,46.013142]);
 
-        myMap.geoObjects.add(myPlacemark);
+            myMap.geoObjects.add(myPlacemark);
 
+        }
     }
-
-
-
-
-
-
-
 
 });
